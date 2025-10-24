520b569ec9b47ae67d67c5e204b888434e5809c9
Charlton Shih <168577923+chewton2k@users.noreply.github.com>
2025-10-24 02:02:22 -0700

    initial setup for commands and database

---
+++ Redis.py
@@ -0,0 +1,155 @@
+from gevent import socket
+from gevent.pool import Pool
+from gevent.server import StreamServer
+
+from collections import namedtuple
+from io import BytesIO
+from socket import error as socket_error
+
+
+# We'll use exceptions to notify the connection-handling loop of problems.
+class CommandError(Exception): pass
+class Disconnect(Exception): pass
+
+Error = namedtuple('Error', ('message',))
+
+
+class ProtocolHandler(object):
+    def __init__(self): 
+        self.handlers = { 
+            '+' : self.handle_simple_string, 
+            '-' : self.handle_error, 
+            ':' : self.handle_integer, 
+            '$' : self.handle_string, 
+            '*' : self.handle_array, 
+            '%' : self.handle_dictionary
+        }
+                
+    def handle_request(self, socket_file):
+        first_byte = socket_file.read(1)
+        if not first_byte: 
+            raise Disconnect() 
+        
+        try: 
+            return self.handlers[first_byte.decode()](socket_file)
+        except: 
+            raise CommandError("Invalid request")
+        
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip('\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n'))
+
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handle_string(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+        if length == -1:
+            return None
+        length += 2
+        return socket_file.read(length)[:-2]
+
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readline().rstrip('\r\n'))
+        elements = [self.handle_request(socket_file)
+                    for _ in range(num_items * 2)]
+        return dict(zip(elements[::2], elements[1::2]))
+
+    def write_response(self, socket_file, data):
+        buffer = BytesIO()
+        self._write(buffer, data)
+        buffer.seek(0)
+        socket_file.write(buffer.getvalue())
+        socket_file.flush()
+
+    def _write(self, buffer, data): 
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        if isinstance(data, bytes):
+            buffer.write('$%s\r\n%s\r\n' % (len(data), data))
+        elif isinstance(data, int):
+            buffer.write(':%s\r\n' % data)
+        elif isinstance(data, Error):
+            buffer.write('-%s\r\n' % socket_error.message)
+        elif isinstance(data, (list, tuple)):
+            buffer.write('*%s\r\n' % len(data))
+            for item in data:
+                self._write(buffer, item)
+        elif isinstance(data, dict):
+            buffer.write('%%%s\r\n' % len(data))
+            for key in data:
+                self._write(buffer, key)
+                self._write(buffer, data[key])
+        elif data is None:
+            buffer.write('$-1\r\n')
+        else:
+            raise CommandError('unrecognized type: %s' % type(data))
+
+
+class Server(object):
+    def __init__(self, host='127.0.0.1', port=31337, max_clients=64):
+        self._pool = Pool(max_clients)
+        self._server = StreamServer(
+            (host, port),
+            self.connection_handler,
+            spawn=self._pool)
+
+        self._protocol = ProtocolHandler()
+        self._kv = {}
+
+        self._commands = self.get_commands() 
+
+    def connection_handler(self, conn, address):
+        
+        socket_file = conn.makefile('rwb')
+
+        # Process client requests until client disconnects.
+        while True:
+            try:
+                data = self._protocol.handle_request(socket_file)
+            except Disconnect:
+                break
+
+            try:
+                resp = self.get_response(data)
+            except CommandError as exc:
+                resp = Error(exc.args[0])
+
+            self._protocol.write_response(socket_file, resp)
+
+
+    def get_commands(self):
+        return {
+            'GET': self.get,
+            'SET': self.set,
+            'DELETE': self.delete,
+            'FLUSH': self.flush,
+            'MGET': self.mget,
+            'MSET': self.mset
+        }
+    
+    def get_response(self, data):
+        if not isinstance(data, list):
+            try:
+                data = data.split()
+            except:
+                raise CommandError('Request must be list or simple string.')
+
+        if not data:
+            raise CommandError('Missing command')
+
+        command = data[0].upper()
+        if command not in self._commands:
+            raise CommandError('Unrecognized command: %s' % command)
+
+        return self._commands[command](*data[1:])
+
+    def run(self):
+        self._server.serve_forever()
